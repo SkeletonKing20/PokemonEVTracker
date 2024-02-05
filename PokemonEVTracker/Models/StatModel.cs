37f829f00ac97380de0a44b0c942524022cc9ff1
SkeletonKing20 <n.bonn2002@gmail.com>
2024-02-05 15:43:12 +0100

    Initial commit

---
+++ PokemonEVTracker/Models/StatModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonEVTracker.Models;
+
+public class StatModel
+{
+    public int HP = 0;
+    public int ATK = 0;
+    public int DEF = 0;
+    public int SP_ATK = 0;
+    public int SP_DEF = 0;
+    public int SPEED = 0;
+}
---
+++ PokemonEVTracker/Program.cs
@@ -0,0 +1,14 @@
+﻿using PokemonEVTracker.Controllers;
+
+namespace PokemonEVTracker;
+
+public static class Program
+{
+    public static void Main()
+    {
+        HTMLScrapeController scraper = new HTMLScrapeController();
+        scraper.JSONExport(scraper.ScrapePokemon());
+        
+        
+    }
+}
